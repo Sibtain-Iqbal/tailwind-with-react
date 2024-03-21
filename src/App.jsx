2350cb29a11de3786b9669ee68026e00a0696276
Sibtain Iqbal <139392522+Sibtain-Iqbal@users.noreply.github.com>
2024-03-20 23:23:50 -0700

    A highly used of logics

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Card from './compomemt/Card'
 import './App.css'
+import Compo from './compomemt/Compo'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const myArr = [1,2,3,4,5] 
+  
+  const Employed = {
+    EmployName : "Sibtain Iqbal",
+    EmployEmail : "Sibtainiqbal8844@gmail.com",
+    Active: "ACTIVE",
+    Title : "backend develooper"
+  }
+  const Designer = {
+    EmployName : "Adil HUssain",
+    EmployEmail : "Sibtain0934@gmail.com",
+    Active: "Not Active",
+    Title : "Graphic Designer"
+  }
+  
   return (
+
     <>
    
-     <h1 className='bg-yellow-300 rounded-xl p-3 text-black'>tailwind css</h1>
+     <h1 className='bg-yellow-300 rounded-xl p-3 text-black mb-6' >tailwind css</h1>
+
+    <Card username="sibtain" buttonTxt = "Click me "  lorem= "Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, debitis?" />
+    <br />
+    <Card username = "Alyananh" buttonTxt= "Visit me "  />
+
+
+    <Compo details = {Employed} Designers = {Designer}  />
+
     </>
   )
 }
